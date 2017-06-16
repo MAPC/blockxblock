89db7301856f4f7405e36f5c739bd2e1db2a7c72
allthesignals <wilburnforce@gmail.com>
2017-06-15 21:42:23 -0400

    Modal login
    Closes #146

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,9 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   currentCity: Ember.inject.service(),
+  isLoggingIn: false,
   actions: {
     invalidateSession: function invalidateSession() {
       this.get('session').invalidate();
+    },
+    authenticate() {
+      let { username, password } = this.getProperties('username', 'password');
+      this.get('session').authenticate('authenticator:devise', username, password)
+        .then(()=> {
+          this.set('isLoggingIn', false);
+        })
+        .catch((reason) => {
+          this.set('errorMessage', reason.error || reason);
+        });
     }
   }
 });

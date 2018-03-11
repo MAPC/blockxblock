58cb58b346d2e18d43a52c92fce1a6faba6fcdc9
allthesignals <wilburnforce@gmail.com>
2018-03-11 19:24:30 -0400

    Added ember-cli-github-pages addon

--- config/environment.js
+++ config/environment.js
@@ -170,6 +170,8 @@ module.exports = function(environment) {
 
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/cityxcity/';
     ENV.locationType = 'auto';
     // ENV.rootURL = '/cityxcity/';
     ENV['ember-cli-mirage'] = {

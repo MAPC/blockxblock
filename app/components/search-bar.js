75591d19717a8c9752e7cdf4b91302fdc846575a
Eric Youngberg <eyoungberg@mapc.org>
2018-05-02 14:05:13 -0400

    Finish search-bar functionality

--- app/components/search-bar.js
+++ app/components/search-bar.js
@@ -3,29 +3,49 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  currentCity: Ember.inject.service(),
+
   classNames: ['component', 'search-bar'],
   tagName: 'div',
   query: '',
 
 
-  inputPlaceholder: Ember.computed('city', function() {
-    const city = this.get('city');
+  places: Ember.computed('model', function() {
+    return this.get('model').rejectBy('location_type', 'District-wide');
+  }),
+  
+
+  inputPlaceholder: Ember.computed('currentCity.city.name', function() {
+    const city = this.get('currentCity.city.name');
     const text = 'Search Places';
 
     return city ? `${text} in ${city}` : text;
   }),
 
 
-  results: Ember.computed('query', 'model', function() {
-    const query = this.get('query');
-    const model = this.get('model');
-    const results = [];
+  results: Ember.computed('query', 'places', function() {
+    const places = this.get('places');
+    let query = this.get('query');
+    let results = [];
 
     if (query.length > 0) {
-             
+      query = query.toLowerCase();
+
+      results = places.filter(place => {
+        return place.get('name').toLowerCase().indexOf(query) !== -1;
+      });
     }
 
     return results;
   }),
+
+
+  actions: {
+    showResult(result) {
+      this.set('query', '');
+
+      this.sendAction('click', result);
+    }
+  },
 
 });
--- app/controllers/cities/city.js
+++ app/controllers/cities/city.js
@@ -1,18 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // currentCity: Ember.inject.service(),
-  // session: Ember.inject.service('session'),
-  // citiesController: Ember.inject.controller('cities'),
-  // actions: {
-  //   transitionTo(selected) {
-  //     let inflector = new Ember.Inflector(Ember.Inflector.defaultRules);
-  //     let route = inflector.pluralize(selected['type']);
-  //     let model = this.store.peekRecord(selected['type'], selected.id);
-  //     this.transitionToRoute(`cities.city.${route}`, model.get('city.id'), model);
-  //   },
-  //   invalidateSession() {
-  //     this.get('session').invalidate();
-  //   }
-  // }
+
+  actions: {
+    showPlace(place)  {
+      this.transitionToRoute('cities.city.places', place);
+    }
+  }
+
 });
